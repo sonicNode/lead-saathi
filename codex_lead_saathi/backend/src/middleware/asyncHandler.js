e908a0edb5564e0d1994f0d9c8b85c2bb83626df
Ankit Mandal <88316393+sonicNode@users.noreply.github.com>
2026-04-01 14:42:53 +0530

    Delete codex_lead_saathi directory

--- codex_lead_saathi/backend/src/middleware/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-function asyncHandler(handler) {
-  return function wrappedHandler(req, res, next) {
-    Promise.resolve(handler(req, res, next)).catch(next);
-  };
-}
-
-module.exports = asyncHandler;
-
--- codex_lead_saathi/backend/src/routes/healthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getHealth } = require("../controllers/healthController");
-
-const router = express.Router();
-
-router.get("/", getHealth);
-
-module.exports = router;
-
